d89e65671f471f8f682c35e5cc0d6b235ea1d529
Greg Brunton <gbrunton@stemtech.com>
2016-07-07 16:50:30 +0100

    Initial Upload - Default MVC App

---
+++ DefaultMVC/DefaultMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DefaultMVC.Startup))]
+namespace DefaultMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
